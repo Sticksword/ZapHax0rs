642ae50530cc546b2527ccd57b3cb5ce50dac52a
Michael Chen <Michael.Chen@zaplabs.com>
2016-12-07 16:11:36 -0800

    blah

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import atexit
 from flask import Flask
 from flask import request
 from flask import make_response
+# for reference: http://flask.pocoo.org/docs/0.11/quickstart/
 
 # Flask app should start in global layout
 app = Flask(__name__)
@@ -43,6 +44,17 @@ def webhook():
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 
 def processRequest(req):
